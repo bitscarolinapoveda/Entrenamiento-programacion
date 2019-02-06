cce66db852cf6231c9f61935288d749354828042
bitssebastiansanchez <Sebastian.sanchez@bitsamericas.com>
2019-02-06 16:36:20 -0500

    actividad poo1 v 1.1

--- php/vehicles/php_poo_1.php
+++ php/vehicles/php_poo_1.php
@@ -1,19 +1,51 @@
 <?php
-#namespace clas\poo;
-include_once "php_poo_2.php";
+namespace php\vehicles;
 
 class Vehicle
 {
 
-    public $brand = "hola";
+    public $brand;
     public $seats;
-    public $fuel_capacity;
+    public $fuelCapacity;
     public $licencePlate;
     protected $fuelLevel;
     protected $currentSpeed;
     protected $state;
 
 
+    #metodos get
+    public function getBrand()
+    {
+        return $this->brand;
+    }
 
-    use Metodos;
+    public function getSeats()
+    {
+        return $this->seats;
+    }
+
+    public function getLicensePlate()
+    {
+        return $this->licencePlate;
+    }
+
+    #metodos set
+    public function setFuelLevel($fuelLevel)
+    {
+        $this->fuelLevel = $fuelLevel;
+    }
+
+    public function setCurrentSpeed($currentSpeed)
+    {
+        $this->currentSpeed = $currentSpeed;
+    }
+
+    public function setState($state)
+    {
+        $this->state = $state;
+    }
+    public function getState()
+    {
+        return $this->state;
+    }
 }
